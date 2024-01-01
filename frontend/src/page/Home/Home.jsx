e6a81cf3604554d77b303fdd3bee5f3c4cdc9081
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2024-01-01 11:52:08 -0500

    fixed nav bar, global css, and public folder

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Nav from "../../components/Nav/Nav";
+import Navigation from "../../components/Navigation/Navigation";
 
 const Home = () => {
   return (
-    <div className="p-0 m-0 border-0" style={"height: 100vh; width: 100vw;"}>
-      <Nav></Nav>
+    <div>
+      <Navigation></Navigation>
     </div>
   );
 };
