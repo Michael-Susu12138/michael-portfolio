9b86b0cf971594ddda2ddc383ab267635916ea36
Michael-Susu12138 <yc4557@nyu.edu>
2024-03-31 03:26:47 -0400

    designed side bar

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import Sidebar from "../../components/SideBar/SideBar";
+import Sidebar from "../../components/Sidebar/Sidebar"
+import ParticlesComponent from "../../components/ParticlesComponent/ParticlesComponent";
+import MyCard from "../../components/Card/MyCard";
+import SocialMediaIcons from "../../components/SocialMediaLinks/SocialMediaIcons";
 
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="app-container">
+      <ParticlesComponent />
       <Sidebar />
+      {/* <SocialMediaIcons></SocialMediaIcons> */}
       <main className="main-content">
-        <section className="about-section">About myself</section>
+        {/* <section className="about-section">About myself</section> */}
+        <MyCard></MyCard>
       </main>
     </div>
   );
